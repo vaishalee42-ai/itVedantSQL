faf893179e5f375c7480a6608fbb8d9cc8cda8dc
vaishalee42-ai <vaishalee42@gmail.com>
2026-07-15 17:40:23 +0530

    alter, primarykey,foreign key, modify

--- Bankingdatabase.sql
+++ Bankingdatabase.sql
@@ -10,3 +10,63 @@ CREATE TABLE Customers (
 );
 
 DESC CUSTOMERS;
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+ALTER TABLE Customers
+ADD DateOfBirth DATE;
+
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+
+DROP TABLE ACCOUNTBRANCHES;
+
+ALTER TABLE Accounts
+ADD CustomerID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE customers
+MODIFY FirstName VARCHAR(50) NOT NULL;
+
+ALTER TABLE CUSTOMERS
+ADD CONSTRAINT uq_Email UNIQUE (Email);
+
+
