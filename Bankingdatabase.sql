e32a977f97decd0b38beaeaac409e3232af46583
vaishalee42-ai <vaishalee42@gmail.com>
2026-07-28 20:00:32 +0530

    updated innerjoin and leftjoin

--- Bankingdatabase.sql
+++ Bankingdatabase.sql
@@ -587,3 +587,49 @@ SELECT
     ) AS NextLoanAmount
 FROM Loans;
 
+use bankingdb;
+
+SELECT
+    a.AccountID, a.AccountType, a.Balance,
+    t.TransactionID,
+    t.TransactionDate,
+    t.TransactionType,
+    t.Amount
+FROM Accounts a
+INNER JOIN Transactions t
+ON a.AccountID = t.AccountID;
+
+SELECT
+    a.AccountID, a.AccountType, a.Balance,
+    t.TransactionID,
+    t.TransactionDate,
+    t.TransactionType,
+    t.Amount
+FROM Accounts a
+LEFT JOIN Transactions t
+ON a.AccountID = t.AccountID;
+
+SELECT
+    a.AccountID, a.AccountType, a.Balance,
+    t.TransactionID,
+    t.TransactionDate,
+    t.TransactionType,
+    t.Amount
+FROM Accounts a
+INNER JOIN Transactions t
+ON a.AccountID = t.AccountID
+WHERE t.TransactionType = 'Deposit';
+
+SELECT
+    a.AccountID, a.AccountType, a.Balance,
+    t.TransactionID,
+    t.TransactionDate,
+    t.TransactionType,
+    t.Amount
+FROM Accounts a
+INNER JOIN Transactions t
+ON a.AccountID = t.AccountID
+WHERE a.Balance > 30000
+ORDER BY a.Balance DESC;
+
+
